027cc529a7adcf0cc4211391845bb12eb8eb2f4a
cerivan <github@cerivan.com>
2015-06-16 17:17:01 +0200

    upudate

--- js/script.js
+++ js/script.js
@@ -31,6 +31,32 @@ $(document).ready(function() {
 		
 		// Fin twitter
 	});
+	$(".nav navbar-nav li").eq(3).click(function(){
+		onDeviceReady();
+		function onDeviceReady() {
+			navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		}
+
+
+
+		function onSuccess(position) {
+		var longitude = position.coords.longitude;
+		var latitude = position.coords.latitude;
+		function initialize() {
+			var mapProp = {
+				center:new google.maps.LatLng(latitude,longitude),
+			zoom:5,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		  };
+		  var map=new google.maps.Map(document.getElementsByClassName("map-recap"),mapProp);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+		}
+
+		function onError() { alert('onError!'); }
+	});
+	
+	
 	
 	
 });
