dc732907b60de630320461ca3ad649c6d7e8f88c
cerivan <github@cerivan.com>
2015-06-16 17:37:44 +0200

    update map

--- js/map.js
+++ js/map.js
@@ -1,25 +1,158 @@
-﻿document.addEventListener("deviceready", onDeviceReady, false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }
-
-
+﻿/**
+ * Created by okalukembi on 03/06/2015.
+ */
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 function onSuccess(position) {
-		var longitude = position.coords.longitude;
-        var latitude = position.coords.latitude;
-       function initialize() {
-		  var mapProp = {
-			center:new google.maps.LatLng(latitude,longitude),
-			zoom:5,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-		  };
-		  var map=new google.maps.Map(document.getElementsByClassName("map-recap"),mapProp);
-		}
-		google.maps.event.addDomListener(window, 'load', initialize);
-    }
+    var current_lat = position.coords.latitude;
+    var current_lng = position.coords.longitude;
+    var secheltLoc = new google.maps.LatLng(current_lat, current_lng);
+    //alert(secheltLoc);
 
-function onError() { alert('onError!'); }
+
+    var styles = [
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#000000" }
+            ]
+        },{
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                { "color": "#45a2c7" },
+                { "weight": 0.7 }
+            ]
+        },{
+            "featureType": "landscape.man_made",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#104568" }
+            ]
+        },{
+            "featureType": "landscape.man_made",
+            "elementType": "labels",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },{
+            "featureType": "landscape.natural.landcover",
+            "stylers": [
+                { "color": "#093349" }
+            ]
+        },{
+            "featureType": "landscape.natural.terrain",
+            "stylers": [
+                { "color": "#05203c" }
+            ]
+        },{
+            "featureType": "poi",
+            "stylers": [
+                { "visibility": "on" },
+                { "color": "#031828" }
+            ]
+        },{
+            "featureType": "road",
+            "elementType": "labels"  },{
+            "featureType": "landscape.natural",
+            "stylers": [
+                { "color": "#093349" }
+            ]
+        },{
+            "featureType": "transit",
+            "stylers": [
+                { "color": "#2d6f3d" },
+                { "weight": 0.8 }
+            ]
+        },{
+            "featureType": "administrative",
+            "stylers": [
+                { "color": "#cccccc" },
+                { "weight": 0.1 }
+            ]
+        },{
+            "featureType": "road.highway",
+            "elementType": "labels.icon",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },{
+            "featureType": "road.arterial",
+            "elementType": "labels",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },{
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                { "weight": 0.1 }
+            ]
+        },{
+            "featureType": "water",
+            "stylers": [
+                { "color": "#508ab9" }
+            ]
+        },{
+        },{
+            "featureType": "transit",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },{
+        }
+    ];
+
+
+    var myMapOptions = {
+        zoom: 16
+        ,center: secheltLoc
+        ,mapTypeId: google.maps.MapTypeId.NORMAL
+    };
+    var theMap = new google.maps.Map(document.getElementById("map_canvas"), myMapOptions);
+    var image = "images/gmaps-marker.png"
+    theMap.setOptions({styles:styles});
+    var marker = new google.maps.Marker({
+        map: theMap,
+        draggable: false,
+        position: new google.maps.LatLng(current_lat, current_lng),
+        visible: true,
+        icon: image,
+        title:'Title' // Title
+    });
+
+    var myOptions = {
+        content: ""
+        ,disableAutoPan: false
+        ,maxWidth: 0
+        ,pixelOffset: new google.maps.Size(-140, -110)
+        ,pixelOffset: new google.maps.Size(140, 110)
+        ,zIndex: null
+        ,boxStyle: {
+            background: "url('tipbox.gif') no-repeat"
+            ,opacity: 0.90
+        }
+        ,closeBoxMargin: "10px 2px 2px 2px"
+        ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
+        ,infoBoxClearance: new google.maps.Size(1, 1)
+        ,isHidden: false
+        ,pane: "floatPane"
+        ,enableEventPropagation: false
+    };
+
+/*    var contentString = '<div class="map_annotation_title">You are here</div>'; //Address on pin click
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    infowindow.open(theMap,marker);
+    google.maps.event.addListener(marker, "click", function (e) {
+        infowindow.open(theMap,marker);
+    });*/
+}
+
+function onError(error)
+{
+    alert(error) ;
+}
--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
 		
 		//faceBook
 		var fbLoginSuccess = function (userData) {
-			alert("UserIf: " + JSON.stringify(userData));
-			console.log("UserInfo: " + JSON.stringify(userData));
+			alert("UserInfo: " + JSON.stringify(userData));
+			
 			facebookConnectPlugin.getAccessToken(function(token) {
 				alert("Token: " + token);
-				console.log("Token: " + token);
+				
 			}, function(err) {
 				alert("Could not get access token: " + err);
 			});
@@ -31,32 +31,6 @@ $(document).ready(function() {
 		
 		// Fin twitter
 	});
-	$(".nav navbar-nav li").eq(3).click(function(){
-		onDeviceReady();
-		function onDeviceReady() {
-			navigator.geolocation.getCurrentPosition(onSuccess, onError);
-		}
 
-
-
-		function onSuccess(position) {
-		var longitude = position.coords.longitude;
-		var latitude = position.coords.latitude;
-		function initialize() {
-			var mapProp = {
-				center:new google.maps.LatLng(latitude,longitude),
-			zoom:5,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-		  };
-		  var map=new google.maps.Map(document.getElementsByClassName("map-recap"),mapProp);
-		}
-		google.maps.event.addDomListener(window, 'load', initialize);
-		}
-
-		function onError() { alert('onError!'); }
-	});
-	
-	
-	
 	
 });
