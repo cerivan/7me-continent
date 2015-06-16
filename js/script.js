f18e42c9ab3cbb68a0748cab0bed3d946fb2d9e2
cerivan <github@cerivan.com>
2015-06-16 21:08:24 +0200

    update

--- js/script.js
+++ js/script.js
@@ -9,10 +9,8 @@ $(document).ready(function() {
 		//faceBook
 		var fbLoginSuccess = function (userData) {
 			alert("UserInfo: " + JSON.stringify(userData));
-			
 			facebookConnectPlugin.getAccessToken(function(token) {
 				alert("Token: " + token);
-				
 			}, function(err) {
 				alert("Could not get access token: " + err);
 			});
