a890bfdd807efcf604bcd5c44bf06eeefdcb7753
cerivan <github@cerivan.com>
2015-06-17 20:32:56 +0200

    update app

--- js/script.js
+++ js/script.js
@@ -4,40 +4,8 @@ $(document).ready(function() {
 	$( ".btn-action-photo" ).click(function(){
 		getImage();
 	});
-	$( ".btn-fbk" ).click(function(){
-		
-		//faceBook
-		var fbLoginSuccess = function (userData) {
-			alert("UserInfo: " + JSON.stringify(userData));
-			facebookConnectPlugin.getAccessToken(function(token) {
-				alert("Token: " + token);
-			}, function(err) {
-				alert("Could not get access token: " + err);
-			});
-		}
-
-		facebookConnectPlugin.login(["public_profile"],
-			fbLoginSuccess,
-			function (error) { alert("" + error) }
-		);
-		
-		// Fin faceBook
-	});
-	
-	$( ".btn-twt" ).click(function(){
-		//Twitter
-		
-		// Fin twitter
-	});
 	
 
 	
 });
 
-
-
-function slide() {
-    window.plugins.nativepagetransitions.slide({
-        "href" : "accueil.html"
-    });
-}
