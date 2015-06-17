d6ef5e0589fef6d1e47eca82470dfe8c5eee2b59
cerivan <github@cerivan.com>
2015-06-17 10:30:05 +0200

    update transition

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ var pictureSource;   // picture source
     function onDeviceReadyCamera() {
         pictureSource=navigator.camera.PictureSourceType;
         destinationType=navigator.camera.DestinationType;
+		
+		window.plugins.nativepagetransitions.slide({
+    // the defaults for direction, duration, etc are all fine
+    "href" : "accueil.html"
+});
 				
     }    
        
