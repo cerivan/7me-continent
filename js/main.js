80d6a10db74e6376aaad02bdc8f0643d93e80cb5
cerivan <github@cerivan.com>
2015-06-17 18:20:17 +0200

    update image

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
-﻿
-
-///////////////////////////////////
-
-
-	var pictureSource;   // picture source
+﻿var pictureSource;   // picture source
     var destinationType; // sets the format of returned value
-    
-
+   
+   // Wait for device API libraries to load
+    //
     document.addEventListener("deviceready",onDeviceReadyCamera,false);
 
-    
+    // device APIs are available
+    //
     function onDeviceReadyCamera() {
-		
         pictureSource=navigator.camera.PictureSourceType;
         destinationType=navigator.camera.DestinationType;
-    }
-    
-    
+				
+    }    
        
     
-	function getImage() {	
-		
-        // Retrieve image file location from specified source
+	function getImage() {
+		// Retrieve image file location from specified source
         navigator.camera.getPicture(uploadPhoto, function(message) {
 				alert('get picture failed');
 			},{
 				quality: 50, 
-				
 				destinationType: Camera.DestinationType.FILE_URI,
 				sourceType: Camera.PictureSourceType.CAMERA
 			}
@@ -47,21 +40,19 @@
         options.mimeType="image/jpeg";
 
         var params = new Object();
+        
 		params.value1 = "test";
 		params.value2 = "param";
         options.params = params;
 		options.chunkedMode = false;
         //alert('transferring...');
-        
-       
 		
 		//console.log(options);
  
  	    var url="http://7eco.cerivan.com/app/post.php";
         var ft = new FileTransfer();
         ft.upload(imageData, url, win, fail, options,true);
-  
-    	
+		   	
     }
  
     function win(r) {
@@ -74,9 +65,5 @@
     function fail(error) {
         alert("An error has occurred: Code = " + error.code);
     }
-
-
 	 
-
-
 
