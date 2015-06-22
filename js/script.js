b9289907dd66087735ff89f7ecb69f169b037dfc
cerivan <github@cerivan.com>
2015-06-22 14:14:12 +0200

    update face book

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ $(document).ready(function() {
 		//faceBook
 		var fbLoginSuccess = function (userData) {
 			alert("UserInfo: " + JSON.stringify(userData));
-			facebookConnectPlugin.getAccessToken(function(token) {
-				alert("Token: " + token);
-			}, function(err) {
-				alert("Could not get access token: " + err);
-			});
-		}
+			facebookConnectPlugin.getLoginStatus(
+				function (status) {
+					alert("current status: " + JSON.stringify(status));
+					
+				}
+			);
+		};
 
 		facebookConnectPlugin.login(["public_profile"],
 			fbLoginSuccess,
