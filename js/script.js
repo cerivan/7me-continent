6eec2bad0707ebac62b5ca760929bcf233c59bd5
cerivan <github@cerivan.com>
2015-09-14 17:14:42 +0200

    update face book

--- js/script.js
+++ js/script.js
@@ -75,21 +75,14 @@ $(document).ready(function() {
 	$( ".btn-fbk" ).click(function(){
 		
 		//faceBook
-			  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1626160201005581',
-      xfbml      : true,
-      version    : 'v2.4'
-    });
-  };
+					var fbLoginSuccess = function (userData) {
+				alert("UserInfo: " + JSON.stringify(userData));
+			}
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+			facebookConnectPlugin.login(["public_profile"],
+				fbLoginSuccess,
+				function (error) { alert("" + error) }
+			);
 		
 		// Fin faceBook
 	});
